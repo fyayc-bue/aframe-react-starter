fa9b2a6ebfbcc515df03ee68199dd80417d384e4
Tilman Porschütz <tip@foryouandyourcustomers.com>
2019-08-22 18:56:22 +0200

    ui mock

--- src/index.js
+++ src/index.js
@@ -4,17 +4,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import { Sidebar, Menu, Icon, Button, Header, SidebarPushable } from 'semantic-ui-react';
+import { Sidebar, Menu, Icon, Button, Container, SidebarPushable, SidebarPusher } from 'semantic-ui-react';
 
 import ViewerScene from './Components/Scenes/ViewerScene/ViewerScene';
 
 class App extends React.Component {
 
-  state = { visible: false }
+  state = { visible: true }
 
   handleHideClick = () => this.setState({ visible: false })
   handleShowClick = () => this.setState({ visible: true })
   handleSidebarHide = () => this.setState({ visible: false })
+  computeButtonClass = (visible) => (visible) ? 'app__controls__button--hidden' : 'app__controls__button';
 
   render() {
 
@@ -22,21 +23,24 @@ class App extends React.Component {
 
     return (
       <main className='app'>
-        
-        {/* <SidebarPushable>
-          <Sidebar 
-            as={Menu}
-            className='app__toolkit'
-            animation='overlay'
-            direction='left'
-            icon='labeled'
-            inverted
-            onHide={this.handleSidebarHide}
-            vertical
-            visible={visible}
-            width='thin'>
-            <Header as='h3'>Toolkit</Header>
 
+        <SidebarPusher>
+          <div class='app__controls'>
+            <Button basic icon='edit outline' className={this.computeButtonClass(visible)} onClick={this.handleShowClick} />
+            <Button basic icon='eye' className={this.computeButtonClass(!visible)} onClick={this.handleHideClick} />
+          </div>
+          <ViewerScene className='app__viewer-scene' />
+        </SidebarPusher>
+
+        <SidebarPushable className='app__sidebars'>
+          <Sidebar
+            as={Menu}
+            className='app__sidebar app__sidebar--toolkit'
+            animation='push'
+            direction='left'
+            vertical
+            visible={visible}>
+            <Menu.Item as='a' header>Toolkit</Menu.Item>
             <Menu.Item as='a'>
               Customer
             </Menu.Item>
@@ -48,23 +52,33 @@ class App extends React.Component {
             </Menu.Item>
 
           </Sidebar>
-          <Sidebar className='app__inspector sidebar'>
-            <Header as='h3'>Inspector</Header>
+          <Sidebar className='app__sidebar app__sidebar--inspector'
+            as={Menu}
+            animation='push'
+            direction='right'
+            vertical
+            visible={visible}>
+            <Menu.Item as='a' header>Inspektor</Menu.Item>
+            <Container text fluid><p>Kein Item ausgewählt!</p></Container>
+            <Menu.Item as='a'>Customer Layer</Menu.Item>
+            <Container text fluid><p>...</p></Container>
+            <Menu.Item as='a'>Experience Layer</Menu.Item>
+            <Container text fluid><p>...</p></Container>
+            <Menu.Item as='a'>Organisation Layer</Menu.Item>
+            <Container text fluid><p>...</p></Container>
+            <Menu.Item as='a'>Performance Layer</Menu.Item>
+            <Container text fluid><p>...</p></Container>
+            <Menu.Item as='a'>Asset Layer</Menu.Item>
+            <Container text fluid><p>...</p></Container>
+            <Menu.Item as='a'>Data Layer</Menu.Item>
+            <Container text fluid><p>...</p></Container>
           </Sidebar>
-        </SidebarPushable> */}
+        </SidebarPushable>
 
-        {/* <Button.Group>
-          <Button disabled={visible} onClick={this.handleShowClick}>
-            Show sidebars
-          </Button>
-          <Button disabled={!visible} onClick={this.handleHideClick}>
-            Hide sidebars
-          </Button>
-        </Button.Group> */}
 
-        <ViewerScene className='app__viewer-scene'/>
-        
-        
+
+
+
       </main>
     );
   }
