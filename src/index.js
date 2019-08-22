f60ca8092fd065fcfa090254626bbc4341301a61
Tilman Porschütz <tip@foryouandyourcustomers.com>
2019-08-22 17:31:43 +0200

    added semantic UI

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
+import { Container, Header } from 'semantic-ui-react';
 
 import ViewerScene from './Components/Scenes/ViewerScene/ViewerScene';
 
@@ -12,13 +13,13 @@ class App extends React.Component {
   render() {
     return (
       <main className='app'>
-        <div className='app__toolkit'>
-          <h3>Corporate Experience Toolkit</h3>
-        </div>
+        <Container className='app__toolkit sidebar'>
+          <Header as='h3'>Corporate Experience Toolkit</Header>
+        </Container>
         <ViewerScene className='app__viewer-scene'/>
-        <div className='app__inspector'>
-          <h3>Information Flow Inspector</h3>
-        </div>
+        <Container className='app__inspector sidebar'>
+          <Header as='h3'>Information Flow Inspector</Header>
+        </Container>
       </main>
     );
   }
