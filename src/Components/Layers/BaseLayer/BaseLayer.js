d44be608988446bd0a4dfb86cca05cceff6262d4
Thomas Koenig <tko@foryouandyourcustomers.com>
2019-08-23 15:33:50 +0200

    Refactor ViewerScene to functional. Re-render works now when uniformScale is updated.

--- src/Components/Layers/BaseLayer/BaseLayer.js
+++ src/Components/Layers/BaseLayer/BaseLayer.js
@@ -1,53 +1,67 @@
-import 'aframe';
-import 'aframe-animation-component';
+import React, { useContext, useState } from "react";
+import { InspectorDispatch } from "../../InspectorDispatch";
 
-import { Entity } from 'aframe-react';
-import React from 'react';
+import "aframe";
+import "aframe-animation-component";
 
-export default class ViewerScene extends React.Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            color: (props.color) ? props.color : 'lightgrey',
-            offset: (props.offset) ? props.offset : 0,
-            uniformScale: (props.uniformScale) ? props.uniformScale : 1,
-            opacity: (props.opacity) ? props.opacity : 1,
-        };
-    }
+import { Entity } from "aframe-react";
 
-    focus() {
-        this.setState({
-            uniformScale: 1.2
-        })
-    }
+const ViewerScene = props => {
+  const { state, dispatch } = useContext(InspectorDispatch);
 
-    render() {
-        const animation = {
-            to:{x:0,y:this.state.offset,z:0},
-            from: {x:0,y:this.state.offset/this.state.level + 9.6,z:0},
-        }
-        console.log(this.state.textureId);
-        return (
-            <Entity
-                position={{ x: 0, y: this.state.offset, z: 0 }}
-                scale={{ x: this.state.uniformScale, y: this.state.uniformScale, z: this.state.uniformScale }}
-                animation=
-                {{
-                    property: 'position',
-                    from: animation.from,
-                    to: animation.to,
-                    loop: false,
-                    easing: 'easeOutElastic',
-                    elasticity: 100,
-                }}>
-                <Entity className='floor'
-                    geometry={{ primitive: 'box', height: .2, width: 10, depth: 10 }}
-                    material={{ color: this.state.color, opacity: this.state.opacity }}
-                    events={{ click: this.focus.bind(this) }}
-                />
-                <Entity position='4.111 0.262 3.729' sprite={{src:'hotspot.png'}}/>
-            </Entity>
-        );
-    }
+  const color = props.color ? props.color : "lightgrey";
+  const offset = props.offset ? props.offset : 0;
+  const uniformScale = props.uniformScale ? props.uniformScale : 1;
+  const opacity = props.opacity ? props.opacity : 1;
+  const level = 1;
+
+  // constructor(props) {
+  //     super(props);
+  //     this.state = {
+  //         color: (props.color) ? props.color : 'lightgrey',
+  //         offset: (props.offset) ? props.offset : 0,
+  //         uniformScale: (props.uniformScale) ? props.uniformScale : 1,
+  //         opacity: (props.opacity) ? props.opacity : 1,
+  //     };
+  // }
+
+//   const focus = () => {
+//     uniformScale = 1.2;
+//   }
+
+  const animation = {
+    to: { x: 0, y: offset, z: 0 },
+    from: { x: 0, y: offset / level + 9.6, z: 0 }
+  };
+
+  //   console.log(this.state.textureId);
+
+  return (
+    <Entity
+      position={{ x: 0, y: offset, z: 0 }}
+      scale={{
+        x: uniformScale,
+        y: uniformScale,
+        z: uniformScale
+      }}
+      animation={{
+        property: "position",
+        from: animation.from,
+        to: animation.to,
+        loop: false,
+        easing: "easeOutElastic",
+        elasticity: 100
+      }}
+    >
+      <Entity
+        className="floor"
+        geometry={{ primitive: "box", height: 0.2, width: 10, depth: 10 }}
+        material={{ color: color, opacity: opacity }}
+        // events={{ click: this.focus.bind(this) }}
+      />
+      <Entity position="4.111 0.262 3.729" sprite={{ src: "hotspot.png" }} />
+    </Entity>
+  );
 }
+
+export default ViewerScene;
